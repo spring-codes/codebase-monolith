c437660cdecfe3c0758a3a3d33390b8f6e8f3fee
cheroliv <cheroliv@laposte.net>
2020-09-17 04:17:33 +0200

    adding integration test GatewayAgenceIntegTest.kt using spring boot container.

--- src/test/kotlin/com/cheroliv/agence/gateway/GatewayAgenceAppTest.kt
+++ src/test/kotlin/com/cheroliv/agence/gateway/GatewayAgenceAppTest.kt
@@ -3,6 +3,8 @@ package com.cheroliv.agence.gateway
 import org.junit.jupiter.api.Test
 import kotlin.test.assertNotNull
 
+
+
 class GatewayAgenceAppTest {
     @Test fun testAppHasAGreeting() {
         assertNotNull(GatewayAgenceApp().greeting,
---
+++ src/test/kotlin/com/cheroliv/agence/gateway/GatewayAgenceIntegTest.kt
@@ -0,0 +1,12 @@
+package com.cheroliv.agence.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class GatewayAgenceIntegTest {
+    @Test
+    fun contextLoads() {
+    }
+}
