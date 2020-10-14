496ab7d4f6212c607032f6b0a5227967968d3e4d
cheroliv <cheroliv@laposte.net>
2020-10-14 15:32:39 +0200

    deleting Constants object and moving those constants to gradle.properties file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,13 @@
-import Build_gradle.Constants.appDockerBaseImage
-import Build_gradle.Constants.cucumberVersion
-import Build_gradle.Constants.dockerHubImageRepo
-import Build_gradle.Constants.dockerHubPasswordKey
-import Build_gradle.Constants.dockerHubUsernameKey
-import Build_gradle.Constants.jsonwebtokenVersion
-import Build_gradle.Constants.junitJupiterVersion
-import Build_gradle.Constants.jvmTargetVersion
-import Build_gradle.Constants.kotlinCompilerOptions
-import Build_gradle.Constants.problemSpringWebfluxVersion
-import Build_gradle.Constants.commonsLang3Version
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-object Constants {
-    const val projectArtifactGroup = "com.cheroliv.saas"
-    const val projectVersion = "0.0.1"
-    const val mainClass = "com.cheroliv.agence.gateway.GatewayAgenceAppKt"
-    const val defaultTaskName = "bootRun"
-    const val jvmTargetVersion = "1.8"
-    const val kotlinCompilerOptions = "-Xjsr305=strict"
-    const val dockerGradleFileConfigPath = "docker.gradle.kts"
-    const val junitJupiterVersion = "5.7.0"
-    const val cucumberVersion = "6.7.0"
-    const val dockerHubUsernameKey = "hub_docker_com_personal_username"
-    const val dockerHubPasswordKey = "hub_docker_com_personal_password"
-    const val dockerHubImageRepo = "cheroliv/agence-gateway"
-    const val appDockerBaseImage = "adoptopenjdk/openjdk15-openj9:jre-15_36_openj9-0.22.0"
-    const val kotlinVersion = "1.4.10"
-    const val jsonwebtokenVersion = "0.11.2"
-    const val problemSpringWebfluxVersion = "0.26.2"
-    const val commonsLang3Version="3.11"
+plugins {
+    kotlin("jvm") version ("1.4.10")
+    kotlin("plugin.jpa") version ("1.3.72")
+    kotlin("plugin.spring") version ("1.4.10")
+    id("io.spring.dependency-management") version ("1.0.10.RELEASE")
+    id("org.springframework.boot") version ("2.3.4.RELEASE")
+    id("com.google.cloud.tools.jib") version ("2.6.0")
 }
 
 buildscript {
@@ -42,31 +20,20 @@ buildscript {
     }
 }
 
-
-plugins {
-    kotlin("jvm") version ("1.4.10")
-    kotlin("plugin.jpa") version ("1.3.72")
-    kotlin("plugin.spring") version ("1.4.10")
-    id("io.spring.dependency-management") version ("1.0.10.RELEASE")
-    id("org.springframework.boot") version ("2.3.4.RELEASE")
-    id("com.google.cloud.tools.jib") version ("2.6.0")
-}
-
-
 repositories {
     mavenCentral()
     jcenter()
 }
 
-group = Constants.projectArtifactGroup
-version = Constants.projectVersion
+group = (properties["project_artifact_group"] as String?)!!
+version = (properties["project_version"] as String?)!!
 
 java.sourceCompatibility = JavaVersion.VERSION_15
 
-defaultTasks(Constants.defaultTaskName)
+defaultTasks(properties["default_task_name"] as String?)
 
 springBoot {
-    mainClassName = Constants.mainClass
+    mainClassName = properties["main_class"] as String?
 }
 
 configurations {
@@ -89,18 +56,18 @@ dependencies {
         exclude("junit", "junit")
     }
     //testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-    testImplementation("io.cucumber:cucumber-java:$cucumberVersion")
-    testImplementation("io.cucumber:cucumber-junit-platform-engine:$cucumberVersion")
-    testImplementation("io.cucumber:cucumber-junit:$cucumberVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
-    testImplementation("org.junit.vintage:junit-vintage-engine:$junitJupiterVersion")
+    testImplementation("io.cucumber:cucumber-java:${properties["cucumber_version"]}")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:${properties["cucumber_version"]}")
+    testImplementation("io.cucumber:cucumber-junit:${properties["cucumber_version"]}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${properties["junit_jupiter_version"]}")
+    testImplementation("org.junit.vintage:junit-vintage-engine:${properties["junit_jupiter_version"]}")
     // add coroutines support
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     // add spring boot support
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web") {
         exclude("org.springframework.boot", "spring-boot-starter-tomcat")
     }
@@ -119,6 +86,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("io.r2dbc:r2dbc-h2")
+//    runtimeOnly("io.r2dbc:r2dbc-spi")
     //runtimeOnly("io.r2dbc:r2dbc-postgresql")
     //runtimeOnly("org.postgresql:postgresql")
 
@@ -130,12 +98,12 @@ dependencies {
         exclude("junit", "junit")
     }
     //JWT
-    implementation("org.zalando:problem-spring-webflux:$problemSpringWebfluxVersion")
-    implementation("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
-    implementation("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
+    implementation("org.zalando:problem-spring-webflux:${properties["problem_spring_webflux_version"]}")
+    implementation("io.jsonwebtoken:jjwt-api:${properties["jsonwebtoken_version"]}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${properties["jsonwebtoken_version"]}")
+    implementation("io.jsonwebtoken:jjwt-jackson:${properties["jsonwebtoken_version"]}")
     // misc
-    implementation ("org.apache.commons:commons-lang3:$commonsLang3Version")
+    implementation("org.apache.commons:commons-lang3:${properties["commons_lang3_version"]}")
 }
 
 tasks.withType<Test> {
@@ -149,20 +117,20 @@ tasks.withType<Test> {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf(kotlinCompilerOptions)
-        jvmTarget = jvmTargetVersion
+        freeCompilerArgs = listOf(properties["kotlin_compiler_options"]) as List<String>
+        jvmTarget = properties["jvm_target"] as String
     }
 }
 
 jib {
     from {
-        image = appDockerBaseImage
+        image = properties["app_docker_base_image"] as String?
     }
     to {
-        image = dockerHubImageRepo
+        image = properties["docker_hub_image_repo"] as String?
         auth {
-            username = project.properties[dockerHubUsernameKey] as String?
-            password = project.properties[dockerHubPasswordKey] as String?
+            username = properties["docker_hub_username_key"] as String?
+            password = properties["docker_hub_password_key"] as String?
         }
     }
 }
